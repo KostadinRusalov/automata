2c41ce92804b421f860b7819b974986a91173cb3
kostadinrusalov <rusalov@uni-sofia.bg>
2023-06-14 10:01:44 +0300

    update algorithm

--- MyStructures/Algorithm.hpp
+++ MyStructures/Algorithm.hpp
@@ -1,21 +1,29 @@
 #pragma once
-
-template<class Iter, class T>
-Iter find(Iter first, Iter last, const T &value) {
-    for (auto it = first; it != last; ++it) {
-        if (value == *it) {
-            return it;
+namespace Algorithm {
+    template<class Iter, class T>
+    Iter find(Iter first, Iter last, const T &value) {
+        for (auto it = first; it != last; ++it) {
+            if (value == *it) {
+                return it;
+            }
         }
+        return last;
     }
-    return last;
-}
 
-template<class Iter, class UnitaryPredicate>
-bool allOf(Iter first, Iter last, UnitaryPredicate p) {
-    for (auto it = first; it != last; ++it) {
-        if (!p(*it)) {
-            return false;
+    template<class Iter, class UnitaryPredicate>
+    bool allOf(Iter first, Iter last, UnitaryPredicate p) {
+        for (auto it = first; it != last; ++it) {
+            if (!p(*it)) {
+                return false;
+            }
         }
+        return true;
     }
-    return true;
+
+    template<class T>
+    void swap(T &rhs, T &lhs) {
+        T copy(rhs);
+        rhs = lhs;
+        lhs = copy;
+    }
 }
