5420c73b9a53fc8b9c1d1e6ce55087544cf97cb5
kostadinrusalov <rusalov@uni-sofia.bg>
2023-06-14 15:21:28 +0300

    move alphabet logic in base class

--- Automata.h
+++ Automata.h
@@ -5,6 +5,20 @@
 class Automata {
 public:
     typedef OrderedSet<char> Alphabet;
+    typedef unsigned State;
+    typedef OrderedSet<State> StateCollection;
+
+protected:
+    Alphabet alphabet;
+
 public:
     virtual ~Automata() = default;
+
+    void setAlphabet(const Alphabet &alphabet);
+
+    void setAlphabet(Alphabet &&alphabet);
+
+    void addSymbol(char s);
+
+    void removeSymbol(char s);
 };
