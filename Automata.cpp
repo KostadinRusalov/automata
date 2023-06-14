d82dbc3450b8ce86554ef07107ca39c2a7b60c7a
kostadinrusalov <rusalov@uni-sofia.bg>
2023-06-14 15:21:17 +0300

    move alphabet logic in base class

---
+++ Automata.cpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "Automata.h"
+
+
+void Automata::setAlphabet(const Alphabet &alphabet) {
+    this->alphabet = alphabet;
+}
+
+void Automata::setAlphabet(Alphabet &&alphabet) {
+    this->alphabet = std::move(alphabet);
+}
+
+void Automata::addSymbol(char s) {
+    alphabet.add(s);
+}
+
+void Automata::removeSymbol(char s) {
+    alphabet.remove(s);
+}
